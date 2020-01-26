4e22a4999f83ed08135f085ac71044f7bde192a4
Steven Jalabert <stevenstrike@users.noreply.github.com>
2020-01-26 15:10:52 +0100

    Update start-server.sh
    Attempt to run xlink kai.
    Added lots of echo's

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,14 +2,27 @@
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
-echo "---Sleep zZz---"
-sleep infinity
+#echo "---Sleep zZz---"
+#sleep infinity
 
+echo "cd to xlink kai data dir: ${DATA_DIR}."
 cd ${DATA_DIR}
-wget -q -nc --show-progress --progress=bar:force:noscroll "${DL_URI}"
+echo "WGet XLink Kai archive: ${DL_URI}"
+wget -q -nc --show-progress --progress=bar:force:noscroll "${DL_URI}."
+
+echo "Decompress XLink Kai Archive."
 tar -xvf kaiEngine*
 rm -R kaiEngine*.tar.gz
+
+echo "cd to xlink kai engine dir."
 cd kaiEngine*
+echo "Move XLink Kai files back to xlink kai data dir."
 mv * ${DATA_DIR}
+echo "cd to xlink kai data dir: ${DATA_DIR}."
 cd ${DATA_DIR}
+
+echo "Remove unused folder kaiEngine-*."
 rm -R kaiEngine-*
+
+echo "Run dumb auto retry XLink Kai Script."
+./runforever.sh
